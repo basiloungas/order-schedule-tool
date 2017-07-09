40dc6f888af07098a55924b191ca88fe91a8ed70
Bill Trikalinos <billtrik@gmail.com>
2017-07-09 14:29:48 +0200

    Setup redux-thunk

--- src/store.js
+++ src/store.js
@@ -1,6 +1,10 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
 import reducers from './reducers';
 
 export default function buildStore() {
-  return createStore(reducers);
+  return createStore(
+    reducers,
+    applyMiddleware(thunk)
+  );
 }
